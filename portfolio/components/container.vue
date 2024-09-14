7b1f0c66ec751e454823b997af552fa8ff26549c
netervati <netervati@gmail.com>
2024-09-14 12:35:37 +0800

    Remove aos
    We really don't need the animations at this point.
    However, let's not remove the package just yet.
    We might later opt to using page transitions
    instead.

--- portfolio/components/container.vue
+++ portfolio/components/container.vue
@@ -7,7 +7,6 @@
 <template>
   <section class="flex flex-col gap-12 py-12 text-center">
     <h2
-      data-aos="fade-up"
       class="font-main mb-12 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:max-2xltext-6xl"
     >
       {{ title }}<span class="text-cyan-500">.</span>
